19a03d651b0fb2ef26f9ae5dcec8a3242552ed25
WANG,Xiao <wangxiao1254@gmail.com>
2017-11-21 14:15:45 -0500

    Update shot.cpp

--- test/shot.cpp
+++ test/shot.cpp
@@ -1,15 +1,39 @@
 #include "test/test.h"
-#include <iostream>
 using namespace std;
 
+void task1(int party, int port);
+
 int main(int argc, char** argv) {
-	int length = 1<<24, port, party;
-	parse_party_and_port(argv, 2, &party, &port);
+
+		int port, party;
+		parse_party_and_port(argv, 2, &party, &port);
+
+
+		if(party == 0) {
+			// this does not work
+			thread t1(task1, ALICE, port);
+
+			//for firewall
+			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+			thread t2(task1, BOB, port);
+
+
+			t1.join();
+			t2.join();
+		}else{
+			//this works (two processes)
+			thread t1(task1, party, 22488);
+			t1.join();
+		}
+}
+
+void task1(int party, int port)
+{
 	NetIO * io = new NetIO(party==ALICE ? nullptr:"127.0.0.1", port);
-	cout <<"NPOT\t"<<10000.0/test_ot<NetIO, OTNP>(io, party, 10000)*1e6<<" OTps"<<endl;
+	int length = 10000;
+	cout <<"NPOT\t"<<10000.0/test_ot<NetIO, OTNP>(io, party, length)*1e6<<" OTps"<<endl;
 	cout <<"Semi Honest OT Extension\t"<<double(length)/test_ot<NetIO, SHOTExtension>(io, party, length)*1e6<<" OTps"<<endl;
 	cout <<"Semi Honest COT Extension\t"<<double(length)/test_cot<NetIO, SHOTExtension>(io, party, length)*1e6<<" OTps"<<endl;
 	cout <<"Semi Honest ROT Extension\t"<<double(length)/test_rot<NetIO, SHOTExtension>(io, party, length)*1e6<<" OTps"<<endl;
 	delete io;
 }
-
